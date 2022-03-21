888ff0c0f23ed1039ed955fc6e35de67a3a82005
aziznal <aziznal.dev@gmail.com>
2022-03-21 18:15:22 +0300

    Added empty file for app theme

---
+++ lib/src/theme/app_theme.dart
@@ -0,0 +1,7 @@
+class AppTheme {
+  static String primaryMember = '';
+  static String primaryCoach = '';
+  static String sidenav = '';
+  static String darkBg = '';
+  static String lightBg = '';
+}
