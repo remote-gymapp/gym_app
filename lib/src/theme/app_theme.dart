3a2eed68e0501bcbd8d61bd501baba12c3008b9c
aziznal <aziznal.dev@gmail.com>
2022-03-22 17:17:27 +0300

    Add main project color values to theme file

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/widgets.dart';
+
 class AppTheme {
-  static String primaryMember = '';
-  static String primaryCoach = '';
-  static String sidenav = '';
-  static String darkBg = '';
-  static String lightBg = '';
+  static Color primaryMember = const Color(0xff3772ff);
+  static Color primaryCoach = const Color(0xffceff1a);
+  static Color sidenav = const Color(0xff10101c);
+  static Color darkBg = const Color(0xff0c1727);
+  static Color lightBg = const Color(0xff1c2939);
 }
