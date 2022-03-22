8cdc924e9c93ecf8f93c55ffe5192cab6d8c26a0
muammertekin <muammertekiinpc@gmail.com>
2022-03-22 17:39:31 +0300

    move flexible widget outside method

--- lib/src/pages/auth/widgets/sign_in_page.dart
+++ lib/src/pages/auth/widgets/sign_in_page.dart
@@ -19,9 +19,9 @@ class SignInPage extends GetView {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              _buildTitleArea(),
-              _buildSignInFormArea(),
-              _buildSignUpTextNavigationArea(),
+              Flexible(flex: 1, child: _buildTitleArea()),
+              Flexible(flex: 2, child: _buildSignInFormArea()),
+              Flexible(flex: 1, child: _buildSignUpTextNavigationArea()),
             ],
           ),
         ),
@@ -29,118 +29,109 @@ class SignInPage extends GetView {
     );
   }
 
-  Flexible _buildTitleArea() {
-    return Flexible(
-      flex: 1,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset('dumbbell.png'),
-          SizedBox(
-            width: 20.0,
+  Widget _buildTitleArea() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset('dumbbell.png'),
+        SizedBox(
+          width: 20.0,
+        ),
+        Text(
+          'Member Login',
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
-          Text(
-            'Member Login',
-            style: TextStyle(
-              fontSize: 25.0,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
-  Flexible _buildSignInFormArea() {
-    return Flexible(
-      flex: 2,
-      child: Form(
-        key: _formKey,
-        child: Flex(
-          direction: Axis.vertical,
-          children: [
-            Flexible(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CustomTextFormField(
-                    prefixIcon: Icon(
-                      Icons.alternate_email,
+  Widget _buildSignInFormArea() {
+    return Form(
+      key: _formKey,
+      child: Flex(
+        direction: Axis.vertical,
+        children: [
+          Flexible(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CustomTextFormField(
+                  prefixIcon: Icon(
+                    Icons.alternate_email,
+                    color: Colors.white,
+                  ),
+                  labelText: 'E-mail',
+                  isPasswordField: false,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomTextFormField(
+                  prefixIcon: Icon(
+                    Icons.vpn_key_sharp,
+                    color: Colors.white,
+                  ),
+                  labelText: 'Password',
+                  isPasswordField: true,
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.visibility,
                       color: Colors.white,
                     ),
-                    labelText: 'E-mail',
-                    isPasswordField: false,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  CustomTextFormField(
-                    prefixIcon: Icon(
-                      Icons.vpn_key_sharp,
-                      color: Colors.white,
-                    ),
-                    labelText: 'Password',
-                    isPasswordField: true,
-                    suffixIcon: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.visibility,
-                        color: Colors.white,
-                      ),
-                      splashRadius: 1,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: CustomButtonWidget(
-                childWidget: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 22.0,
+                    splashRadius: 1,
                   ),
                 ),
-                onPressed: () {},
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: CustomButtonWidget(
+              childWidget: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 22.0,
+                ),
               ),
-            )
-          ],
-        ),
+              onPressed: () {},
+            ),
+          )
+        ],
       ),
     );
   }
 
-  Flexible _buildSignUpTextNavigationArea() {
-    return Flexible(
-      flex: 1,
-      child: Wrap(
-        direction: Axis.vertical,
-        alignment: WrapAlignment.center,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: [
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              'Sign Up',
-              style: TextStyle(
-                color: Color(0XFF3772FF),
-                fontSize: 23,
-              ),
-            ),
-          ),
-          Text(
-            'for a new account',
+  Widget _buildSignUpTextNavigationArea() {
+    return Wrap(
+      direction: Axis.vertical,
+      alignment: WrapAlignment.center,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            'Sign Up',
             style: TextStyle(
-              color: Color(0XFFFFFFFF),
-              fontSize: 20.0,
+              color: Color(0XFF3772FF),
+              fontSize: 23,
             ),
           ),
-        ],
-      ),
+        ),
+        Text(
+          'for a new account',
+          style: TextStyle(
+            color: Color(0XFFFFFFFF),
+            fontSize: 20.0,
+          ),
+        ),
+      ],
     );
   }
 }
