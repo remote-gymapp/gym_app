d2f766225e45ca962efcfdb2f6460c40bd7546d8
muammertekin <muammertekiinpc@gmail.com>
2022-03-26 17:18:28 +0300

    Refactor sign up page design

--- lib/src/pages/auth/widgets/sign_up_page.dart
+++ lib/src/pages/auth/widgets/sign_up_page.dart
@@ -22,9 +22,113 @@ class SignUpPage extends GetView {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Flexible(flex: 1, child: _buildTitleArea()),
-              Flexible(flex: 2, child: _buildSignUpFormArea()),
-              Flexible(flex: 1, child: _buildSignUpTextNavigationArea()),
+              Flexible(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset('dumbbell.png'),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'Member Sign Up',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('Fill in form information in the fields below'),
+                    CustomTextFormField(
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                      labelText: 'Name',
+                    ),
+                    CustomTextFormField(
+                      prefixIcon: Icon(
+                        Icons.alternate_email,
+                        color: Colors.white,
+                      ),
+                      labelText: 'E-mail',
+                    ),
+                    CustomTextFormField(
+                      prefixIcon: Icon(
+                        Icons.vpn_key_sharp,
+                        color: Colors.white,
+                      ),
+                      labelText: 'Password',
+                      isPasswordField: true,
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.visibility,
+                          color: Colors.white,
+                        ),
+                        splashRadius: 1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      value: true,
+                      onChanged: (value) {},
+                      activeColor: Colors.white,
+                    ),
+                    Text(
+                      'I agree to all terms and conditions',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: CustomButtonWidget(
+                  childWidget: Text(
+                    'Create Account',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: TextButton(
+                  onPressed: () {
+                    Get.toNamed(ScreenRoute.signInPage);
+                  },
+                  child: Text(
+                    'I already have an account',
+                    style: TextStyle(
+                      color: Color(0XFF8D8D8D),
+                      fontSize: 16,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -32,7 +136,7 @@ class SignUpPage extends GetView {
     );
   }
 
-  Widget _buildTitleArea() {
+  /*  Widget _buildTitleArea() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -53,7 +157,7 @@ class SignUpPage extends GetView {
     );
   }
 
-  Widget _buildSignUpFormArea() {
+  */ /*  Widget _buildSignUpFormArea() {
     return Form(
       key: _formKey,
       child: Flex(
@@ -145,4 +249,6 @@ class SignUpPage extends GetView {
       ),
     );
   }
+
+ */
 }
